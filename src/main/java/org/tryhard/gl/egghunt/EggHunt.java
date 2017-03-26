075aa861cff7dd2615c751067b9e3797e2138b30
Porrow <porrow92@gmail.com>
2017-03-26 06:20:12 +0200

    First Commit

---
+++ src/main/java/org/tryhard/gl/egghunt/EggHunt.java
@@ -0,0 +1,13 @@
+package org.tryhard.gl.egghunt;
+
+/**
+ * Hello world!
+ *
+ */
+public class EggHunt 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/tryhard/gl/egghunt/EggHuntTest.java
@@ -0,0 +1,38 @@
+package org.tryhard.gl.egghunt;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class EggHuntTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public EggHuntTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( EggHuntTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
