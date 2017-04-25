c26c8cb45b8d668b94aab24863a004246c42b05c
sauceaubeurre <romain.seusse@gmail.com>
2017-04-25 23:38:22 +0200

    ajout classe selection

---
+++ src/main/java/org/tryhard/gl/egghunt/Selection.java
@@ -0,0 +1,28 @@
+package org.tryhard.gl.egghunt;
+
+import java.awt.Graphics2D;
+
+import org.tryhard.gl.egghunt.gui.Window;
+
+public class Selection extends GraphicObject {
+
+	protected static int ID = 1;
+	
+	protected Selection() {
+		super(0, 0, Window.WIDTH, Window.HEIGHT);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void paint(Graphics2D g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void calculate() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
